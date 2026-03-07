364df9391702c625fcf5a8eb8c5e22b5798282ba
Daoud Tajeldeinn <daoudtajeldeinn@google>
2026-03-07 21:01:16 +0200

    Update Firebase project configuration to sudanqualityplatform-11938593

--- OneDrive/Desktop/منصة السودان للجودة/frontend/src/firebase/config.js
+++ OneDrive/Desktop/منصة السودان للجودة/frontend/src/firebase/config.js
@@ -4,9 +4,9 @@ import { getAuth } from "firebase/auth";
 // Sudan Quality Platform Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCtbCNpk39MjhZkaVPOKjiovBexuO3_W_o",
-  authDomain: "decisive-octane-472816-d3.firebaseapp.com",
-  projectId: "decisive-octane-472816-d3",
-  storageBucket: "decisive-octane-472816-d3.firebasestorage.app",
+  authDomain: "sudanqualityplatform-11938593.firebaseapp.com",
+  projectId: "sudanqualityplatform-11938593",
+  storageBucket: "sudanqualityplatform-11938593.firebasestorage.app",
   messagingSenderId: "338906119415",
   appId: "1:338906119415:web:2767a85e29b0b5b1a727f2",
   measurementId: "G-66V5HKTPD0"
