8d2002286ad18308c0dbd8274dbc641a7dc210bc
Daoud Tajeldeinn Ahmed <daoudtajeldeinn@gmail.com>
2026-03-16 16:19:27 +0200

    Delete start-backend.ps1

--- OneDrive/Desktop/منصة السودان للجودة/start-backend.ps1
+++
@@ -1,3 +0,0 @@
-$env:MONGODB_URI = "mongodb+srv://daoudtajeldeinn113_db_user:HbdStyeaJyk5DaVz@sudanqualityplatform.xmr9cgw.mongodb.net/?appName=SUDANQUALITYPLATFORM&retryWrites=true&w=majority"
-npm start
-
