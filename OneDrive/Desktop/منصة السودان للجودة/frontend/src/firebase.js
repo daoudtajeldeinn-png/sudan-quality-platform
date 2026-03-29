50210f4bbcc8bee9b5615ac2372c8f3cea6fab6f
lenovo <lenovo@DESKTOP-HDF4QB0>
2026-03-29 22:48:09 +0300

    Unify firebase exports, add verify route, update .gitignore

--- OneDrive/Desktop/منصة السودان للجودة/frontend/src/firebase.js
+++
@@ -1,7 +0,0 @@
-import { getFirestore } from 'firebase/firestore';
-import app, { auth } from './firebase/config';
-
-// Export Firestore instance
-export const db = getFirestore(app);
-
-export { auth, app };
