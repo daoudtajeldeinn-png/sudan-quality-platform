3af0ad5719273bc6a6d7ecf454ed4e1fb0db9e56
Daoud Tajeldeinn Ahmed <daoudtajeldeinn@gmail.com>
2026-04-26 05:28:56 +0200

    Fix: resolve merge conflict

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/',
   build: {
     outDir: 'dist',
@@ -14,7 +13,3 @@ export default defineConfig({
     include: ['firebase/auth', 'firebase/app']
   }
 })
-
-=======
-})
->>>>>>> 4b7a20e946e57a19d6e3dd5af9abbec206e3e211
